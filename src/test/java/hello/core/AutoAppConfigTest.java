2b1f95a6feac3b8abd4b07747041059d72493dee
jun <mazino0588@gmail.com>
2025-01-03 21:48:12 +0900

    컴포넌트 스캔

---
+++ src/test/java/hello/core/AutoAppConfigTest.java
@@ -0,0 +1,17 @@
+package hello.core;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertInstanceOf(MemberService.class, memberService);
+    }
+}
